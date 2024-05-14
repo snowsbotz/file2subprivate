bd71208473ab5f9dd6a35b5cdec7ad2e23a536f4
TEAM UHD <128911148+snowsbotz@users.noreply.github.com>
2024-05-14 11:45:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -102,6 +102,7 @@ async def start_command(client: Client, message: Message):
                             )
                         ]
                     ]
+                )
             try:
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
