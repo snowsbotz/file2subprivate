d5931b5837eb6774cc9b0edacc1e3ae177b7955a
TEAM UHD <128911148+snowsbotz@users.noreply.github.com>
2024-05-14 12:39:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -76,7 +76,7 @@ async def start_command(client: Client, message: Message):
             if bool(CUSTOM_CAPTION) & bool(msg.document):
                 caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
             else:
-                caption = "" if not msg.caption else msg.caption.html
+                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
 
             if DISABLE_CHANNEL_BUTTON:
                 reply_markup = msg.reply_markup
