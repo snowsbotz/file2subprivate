2e636af7751b989e3172c41c2e2cf0f6180f9c59
TEAM UHD <128911148+snowsbotz@users.noreply.github.com>
2024-05-14 09:11:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from bot import Bot
+from bot import Bot, APP_ID, API_HASH
 
 Bot().run()
+
+owner_client = Client("owner_session", api_id=APP_ID, api_hash=API_HASH)
+
+
+owner_client.start()
+owner_client.run()
